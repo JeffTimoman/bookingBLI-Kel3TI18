8777a1bdad0ec27213fd3a2c66cc207e541cf6de
GroHaul <gavingeraldo.2005@gmail.com>
2025-02-06 08:46:30 +0700

    Push Autentikasi login User
    login user

--- bookingBLI/routes/web.php
+++ bookingBLI/routes/web.php
@@ -2,9 +2,18 @@
 
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SessionController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('user', UserController::class);
+route::resource('user', UserController::class);
+
+Route::get('/home', function () {
+    return view('landing');
+});
+
+Route::get('/session', [SessionController::class, 'index']);
+Route::post('/session/login', [SessionController::class, 'login']);
+
